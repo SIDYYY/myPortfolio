1d6df9398820b7cae5170c22fdb00ec6016daa07
Carl <carldaguinotas@gmail.com>
2024-08-04 18:40:04 +0800

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import Timer from './projects/timer';
 import Que from './projects/que';
@@ -10,17 +11,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/timer" element={<Timer />} />
         <Route path="/que" element={<Que />} />
         <Route path="/calc" element={<Calc />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
 reportWebVitals();
-
 
