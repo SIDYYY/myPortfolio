b41c07f9f6d2923f5c5c0ca62a62e9147b9e660e
Carl <carldaguinotas@gmail.com>
2024-08-04 18:43:01 +0800

    v3

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Timer from './projects/timer';
 import Que from './projects/que';
 import Calc from './projects/cal';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router basename="/myPortfolio">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/timer" element={<Timer />} />
         <Route path="/que" element={<Que />} />
         <Route path="/calc" element={<Calc />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
-reportWebVitals();
 
 
