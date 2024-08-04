73cad827512b155d95f1476120d22da13068ddf6
Carl <carldaguinotas@gmail.com>
2024-08-04 17:42:39 +0800

    vv

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/myPortfolio'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/timer" element={<Timer />} />
